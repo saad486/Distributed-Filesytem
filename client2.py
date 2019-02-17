29289f545983d453951a888cbd1434c8834c49c3
saad486 <34566634+saad486@users.noreply.github.com>
2019-02-17 15:49:08 +0500

    Add files via upload

--- client2.py
+++ client2.py
@@ -1,7 +1,8 @@
+import clientConfig
+
 import socket
-HOST = '192.168.1.103'
+HOST = clientConfig.CLIENT_CONFIG[0]
 import subprocess
-import config
 
 serverFileList = []
 
@@ -21,14 +22,39 @@ def saveFile(sock, file):
 
 def uploadFile(sock, fileName, msg=''):
 	sendVariable ='upload '+fileName
-	if msg.decode() == 'send':
-		f = open(fileName,'rb')
-		l = f.read(1024)
-		while (l):
-			sock.sendall(l)
+	if msg.decode() == 'Local':
+		sock.sendall('ok'.encode())
+		msg = sock.recv(1024)
+		if msg == 'send'.encode():
+			f = open(fileName,'rb')
 			l = f.read(1024)
-		f.close()
-		sock.send('EOF'.encode())
+			while (l):
+				sock.sendall(l)
+				l = f.read(1024)
+			f.close()
+			sock.send('EOF'.encode())
+			return None
+	else:
+		sock.sendall('ok'.encode())
+		msg = sock.recv(1024)
+		addr = msg.decode()
+		b = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		b.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		b.connect((addr,6666))
+		b.sendall(sendVariable.encode())
+		msgFromAnotherServer = b.recv(1024)
+		if msgFromAnotherServer.decode() == 'Local':
+			b.sendall('ok'.encode())
+			msgFromAnotherServer = b.recv(1024)
+			if msgFromAnotherServer.decode() == 'send':
+				f = open(fileName,'rb')
+				l = f.read(1024)
+				while (l):
+					b.sendall(l)
+					l = f.read(1024)
+				f.close()
+				b.send('EOF'.encode())
+				return b
 #-------------------------------------------
 
 #---------------------------------------
@@ -53,59 +79,85 @@ def createFiles(sock,fileName, msg=''):
 print("Enter the port no:\n")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-PORT = input("prompt:\n")
-s.connect((HOST, int(PORT)))
+s.connect((HOST,clientConfig.CLIENT_CONFIG[1] ))
 
 while True:
 	data = input("prompt:")
-	s.sendall(data.encode())
-	msg = s.recv(1024)
-	print(msg,'client')
-	print(msg)
-	if not msg:
-		s.close()
-		break
+	if data == '':
+		print('Type Error! Empty data string')
 
-	listOfData = data.split(' ')
+	else:
+		s.sendall(data.encode())
+		msg = s.recv(1024)
+		if not msg:
+			s.close()
+			break
 
-	if listOfData[0] == 'ls':
-		print(msg.decode())
+		listOfData = data.split(' ')
 
-	elif listOfData[0] == 'download':
-		if msg.decode() == 'Not local':
-			s.send('ok'.encode())
-			addr = s.recv(1024)
+		if listOfData[0] == 'ls':
+			print(msg.decode()+'\n')
 
-			b = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			b.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-			b.connect((addr,6666))
-			b.sendall(data.encode())
-			msgFromAnotherServer = b.recv(1024)
+		elif listOfData[0] == 'download':
+			if msg.decode() == 'Not local':
+				s.send('ok'.encode())
+				addr = s.recv(1024)
 
-			if msgFromAnotherServer.decode() == 'local':
-				print('Got the file remotely')
-				saveFile(b, listOfData[1])
+				b = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				b.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+				b.connect((addr,6666))
+				b.sendall(data.encode())
+				msgFromAnotherServer = b.recv(1024)
+
+				if msgFromAnotherServer.decode() == 'local':
+					saveFile(b, listOfData[1])
+					proc = subprocess.Popen(['leafpad', listOfData[1]])
+					proc.wait()
+						##save in the list
+					serverFileList.append([addr,listOfData[1]])
+					s.close()
+					s = b
+
+			elif msg.decode() == 'local':
+				saveFile(s,listOfData[1])
 				proc = subprocess.Popen(['leafpad', listOfData[1]])
 				proc.wait()
-					##save in the list
-				serverFileList.append([addr,listOfData[1]])
-				s.close()
-				s = b
+				serverFileList.append([HOST,listOfData[1]])
 
-		elif msg.decode() == 'local':
-			print('Got the file locally')
-			saveFile(s,listOfData[1])
+			else:
+				print(msg.decode()+'\n')
+
+		elif listOfData[0] == 'create':
 			proc = subprocess.Popen(['leafpad', listOfData[1]])
 			proc.wait()
-			serverFileList.append([HOST,listOfData[1]])
+			createFiles(s,listOfData[1],msg)
 
-	elif listOfData[0] == 'create':
-		proc = subprocess.Popen(['leafpad', listOfData[1]])
-		proc.wait()
-		createFiles(s,listOfData[1],msg)
+		elif listOfData[0] == 'upload':
+			proc = subprocess.Popen(['leafpad', listOfData[1]])
+			proc.wait()
+			tempSock = uploadFile(s,listOfData[1],msg)
+			if tempSock != None:
+				s = tempSock
 
-	elif listOfData[0] == 'upload':
-		print('here upload')
-		proc = subprocess.Popen(['leafpad', listOfData[1]])
-		proc.wait()
-		uploadFile(s,listOfData[1],msg)
+		elif listOfData[0] == 'delete':
+			if msg.decode() == 'Local':
+				s.sendall('ok'.encode())
+
+			elif msg.decode() == 'Not local':
+				s.sendall('ok'.encode())
+				ret = s.recv(1024)
+				addr = ret.decode()
+				b = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				b.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+				b.connect((addr,6666))
+				b.sendall(data.encode())
+
+				ret = b.recv(1024)
+				if ret.decode() == 'Local':
+					b.sendall('ok'.encode())
+					s = b
+			else:
+				print(msg.decode()+'\n')
+
+		else:
+			print(msg.decode()+'\n')
---
+++ clientConfig.py
@@ -0,0 +1,6 @@
+CLIENT_CONFIG = {
+
+	0 : 'Ip Address',  #ip for the server to connect to
+    1 : 6666     ##Port address of the servers #default 6666
+
+}
